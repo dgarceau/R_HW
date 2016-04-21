2214a715ea44891bf9ce2173e2d6ed5f6438937b
DANIELLE GARCEAU <DGARC054@UCR.EDU>
2016-04-20 22:50:45 -0700

    reverse complementer forward loop

--- HW5.R
+++ HW5.R
@@ -36,5 +36,31 @@ reversecomplementer(x, "comp")
 
 
 sequencevector <- c("ATGCGATCGGGCTAGGCT", "GTGGGCAAGATAGC", "GGGAAATTCCTGATCCTAG")
-# Here I created a vector of 3 different nucleotide sequences 
+# Here I created a vector of 3 different nucleotide sequences by concatenating 3 sequences
+# together 
+
+revcomploop <- NULL
+# Here I created an empty vector to deposit my reverse complemented sequences into later
+for(i in seq(sequencevector[])) {
+  # here I opened a forward loop that will perform functions on the variable i, which in this case are
+  # individual nucleotides, within the vector named sequencevector 
+  revcomplement2 <- function(i){
+  revcomplement2 <- chartr("ATGC", "TACG", paste(rev(substring(i, 1:nchar(i), 1:nchar(i)) ), collapse="")) 
+    return(revcomplement2) 
+  # here I am again defining the function for reverse complementing a sequence as described above
+  # in this instance however, I am using the variable i instead of x becuase i refers to each
+  # character in a sequence within my vector whereas before x referred to the only sequence present 
+  # in the sequence I named x
+}
+  revcomploop <- c(revcomploop, revcomplement2(sequencevector[i]))
+  # here I concatenated the reversed sequences from sequencevector that I generated with the 
+  # forward loop of my reversecomplement function with the revcomploop empty vector I made earlier
+  # in order to deposit these reverse complemented sequences into the empty vector, creating a new
+  # vector with the reverse complemented sequences 
+}
+  
+revcomploop
+sequencevector
+# here I visualized my reverse complemented sequences and my original sequences 
+
 
